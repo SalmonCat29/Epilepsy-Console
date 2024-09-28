4971451dcfc93f489faf35104335527c21da4ff8
SalmonCat29 <sammi22992012@gmail.com>
2024-09-29 09:58:21 +1000

    Add files via upload

---
+++ Epilepsy.cmd
@@ -0,0 +1,111 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCyDJGyX8VAjFB5VSAGMAE+/Fb4I5/jHxumIrF4hVu0ofZ2V07eBQA==
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSjk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFB5VSAGMAE+/Fb4I5/jHyvqOrkgNR/ttNorD39Q=
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+@echo off
+title hope you dont have epelipsy ;)
+mode 200, 50
+:start
+color 10
+color 20
+color 30
+color 40
+color 50
+color 60
+color 70
+color 80
+color 90
+color A0
+color b0
+color c0
+color d0
+color e0
+color f0
+goto :second
+
+:second
+color 30
+color 80
+color 90
+color A0
+color b0
+color c0
+color d0
+color e0
+color f0
+color 40
+color 50
+color 60
+color e0
+color 90
+color A0
+color b0
+color f0
+color 40
+color 50
+color 60
+color 70
+color 70
+color 30
+color 40
+color 50
+color 60
+color 70
+goto :third
+
+:third
+color d0
+color e0
+color 90
+color A0
+color b0
+color f0
+color 40
+color 50
+color 60
+color 70
+color 30
+color 40
+color 30
+color 40
+color 50
+color 60
+color 70
+color 80
+color 90
+color A0
+color b0
+color c0
+color d0
+color e0
+color f0
+goto :start
